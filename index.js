94651fc3574b08fbe67a88283abb47ebcb480835
Gopal Shah <simplecooldude1@gmail.com>
2016-10-28 19:52:31 -0700

    remove log from index

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ var app = express();
 
 app.use(express.static('assets'));
 
-var log = (log, color) => {
-  console.log(hexu[color](log));
-}
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
