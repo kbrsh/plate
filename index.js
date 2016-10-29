f9bf7ba7f5ccd590d06eb386562a601d54b7dd98
Gopal Shah <simplecooldude1@gmail.com>
2016-10-28 19:51:07 -0700

    hexu

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var hexu = require("hexu");
 var app = express();
 
 app.use(express.static('assets'));
+
+var log = (log, color) => {
+  console.log()
+}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
